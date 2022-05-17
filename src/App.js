86c38a7cddd0969e81f9c839fcc19000cf75ff3a
rishabhsuyal <rishabhsuyal2001@gmail.com>
2022-05-17 23:28:42 +0530

    fix2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Homepage from './Homepage';
 
 function App() {
     return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}> 
       <Routes>
           <Route path="/" element={<Homepage/>}/>
       </Routes>
